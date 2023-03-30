aab67ea9189aa0650784d9872933679b174011d8
cheraxm <109953279+cheraxm@users.noreply.github.com>
2023-03-30 23:50:18 +0700

    Add files via upload

---
+++ week8/lab8/Bank.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8;
+
+/**
+ *
+ * @author chuti
+ */
+public class Bank {
+    private Account acct[];
+    private int numAcct;
+    public Bank(){
+        acct = new Account[10];
+    }
+    public void addAccount(Account ac){
+        for(int i = 0; i<10; i++){
+            acct[i] = ac;
+        }
+        numAcct++;
+    }
+    public Account getAccount(int index){
+        return acct[index];
+    }
+    public int getNumAccount(){
+        return numAcct;
+    }
+    
+}
+    
+
